ef8e20c7dfa7ea33b832ac32922487dfc3f8f4d4
Minsun Kim <minsun2767@naver.com>
2016-09-26 20:28:51 +0900

    #1 draw Image and Transform Image

---
+++ app/src/main/java/kr/hs/emrim/minsun2767/imagetest/MainActivity.java
@@ -0,0 +1,41 @@
+package kr.hs.emrim.minsun2767.imagetest;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyImage(getApplicationContext()));
+    }
+
+    class MyImage extends View {
+        MyImage(Context context){
+            super(context);
+
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            Bitmap picture= BitmapFactory.decodeResource(getResources(),R.drawable.hq);
+            float cx=getWidth()/2.0f;
+            float cy=getHeight()/2.0f;
+
+            float x=(getWidth()-picture.getWidth())/2.0f;
+            float y=(getHeight()-picture.getHeight())/2.0f;
+            //canvas.rotate(45,cx,cy); 회전
+            //canvas.translate(-150,200); 이동
+            //canvas.scale(2,2,cx,cy);
+            canvas.skew(0.4f,0.4f);
+            canvas.drawBitmap(picture,x,y,null);
+        }
+    }
+}
