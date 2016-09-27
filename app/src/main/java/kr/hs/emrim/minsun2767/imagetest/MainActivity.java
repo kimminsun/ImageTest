cf4e93db5a3cec0c57589f5531aaaf2d82b019c6
Minsun Kim <minsun2767@naver.com>
2016-09-27 20:22:24 +0900

    #4 Event Programming

--- app/src/main/java/kr/hs/emrim/minsun2767/imagetest/MainActivity.java
+++ app/src/main/java/kr/hs/emrim/minsun2767/imagetest/MainActivity.java
@@ -7,35 +7,56 @@ import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyImage myImg;
+    static final int ORIGINAL=0;
+    static  final int ROTATE=1;
+    static final int TRANSLATE=2;
+    static final int SCALE=3;
+    static final int SKEW=4;
+    int choose=ORIGINAL;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout linear1=(LinearLayout)findViewById(R.id.linear1);
+        myImg=new MyImage(getApplicationContext());
+        linear1.addView(myImg);
     }
 
-    /*class MyImage extends View {
-        MyImage(Context context){
-            super(context);
-
+    public void transformImage(View v){
+        switch (v.getId()){
+            case R.id.but_rotate:
+                choose=ROTATE;
+                break;
+            case R.id.but_translate:
+                choose=TRANSLATE;
+                break;
+            case R.id.but_scale:
+                choose=SCALE;
+                break;
+            case R.id.but_skew:
+                choose=SKEW;
+                break;
         }
+        myImg.setChoose(choose);
+        myImg.invalidate();
+    }
 
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-            Bitmap picture= BitmapFactory.decodeResource(getResources(),R.drawable.hq);
-            float cx=getWidth()/2.0f;
-            float cy=getHeight()/2.0f;
-
-            float x=(getWidth()-picture.getWidth())/2.0f;
-            float y=(getHeight()-picture.getHeight())/2.0f;
-            //canvas.rotate(45,cx,cy); 회전
-            //canvas.translate(-150,200); 이동
-            //canvas.scale(2,2,cx,cy);
-            canvas.skew(0.4f,0.4f);
-            canvas.drawBitmap(picture,x,y,null);
-        }
-    }*/
+//        protected void onDraw(Canvas canvas){
+//            super.onDraw(canvas);
+//            Bitmap picture= BitmapFactory.decodeResource(getResources(), R.drawable.puppy);
+//            float cx=getWidth()/2.0f;
+//            float cy=getHeight()/2.0f;
+//            float x=(getWidth()-picture.getWidth())/2.0f;
+//            float y=(getHeight()-picture.getHeight())/2.0f;
+//            //canvas.rotate(45,cx,cy); 회전이동
+//            canvas.translate(-150,200);
+//            canvas.drawBitmap(picture,x,y,null);
+//        }
 }
--- app/src/main/java/kr/hs/emrim/minsun2767/imagetest/MyImage.java
+++ app/src/main/java/kr/hs/emrim/minsun2767/imagetest/MyImage.java
@@ -10,23 +10,46 @@ import android.view.View;
  * Created by appcreator34 on 2016. 9. 26..
  */
 public class MyImage extends View{
+    int choose=MainActivity.ORIGINAL;
+    Bitmap picture;
     MyImage(Context context){
         super(context);
+        picture= BitmapFactory.decodeResource(getResources(),R.drawable.hq);
+
+    }
+    public void setChoose(int choose){
+        this.choose=choose;
 
     }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Bitmap picture= BitmapFactory.decodeResource(getResources(),R.drawable.hq);
+
         float cx=getWidth()/2.0f;
         float cy=getHeight()/2.0f;
 
         float x=(getWidth()-picture.getWidth())/2.0f;
         float y=(getHeight()-picture.getHeight())/2.0f;
-        //canvas.rotate(45,cx,cy); 회전
-        //canvas.translate(-150,200); 이동
-        //canvas.scale(2,2,cx,cy);
-        canvas.skew(0.4f,0.4f);
+
+        switch (choose){
+            case MainActivity.ROTATE:
+                canvas.rotate(45,cx,cy);
+                break;
+            case MainActivity.TRANSLATE:
+                canvas.translate(-150,200);
+                break;
+            case MainActivity.SCALE:
+                canvas.scale(2,2,cx,cy);
+                break;
+            case MainActivity.SKEW:
+                canvas.skew(0.4f,0.4f);
+                break;
+        }
+        // 회전
+        //이동
+        //
+
         canvas.drawBitmap(picture,x,y,null);
+        //picture.recycle();
     }
 }
